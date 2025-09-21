7e39f9e85f5ee9b4964fecc3f6e759e08ef72de2
emiya_ubw <a6m3.zero.f@gmail.com>
2025-09-22 08:10:27 +0900

    整形

--- Project/source/OptionWindow.cpp
+++ Project/source/OptionWindow.cpp
@@ -1,1 +1,232 @@
 ﻿#include "OptionWindow.hpp"
+
+void OptionWindow::UpdateColumnStr() noexcept {
+	auto* pOption = OptionParam::Instance();
+	switch (this->m_NowSelectTab) {
+	case 0:
+	{
+		this->m_Param[0].m_Str = std::to_string(pOption->GetParam("MasterVolume")->GetSelect());
+		this->m_Param[1].m_Str = std::to_string(pOption->GetParam("BGMVolume")->GetSelect());
+		this->m_Param[2].m_Str = std::to_string(pOption->GetParam("SEVolume")->GetSelect());
+	}
+	break;
+	case 1:
+	{
+		this->m_Param[0].m_Str = pOption->GetParam("WindowMode")->GetValueNow();
+		this->m_Param[1].m_Str = pOption->GetParam("VSync")->GetValueNow();
+		this->m_Param[2].m_Str = pOption->GetParam("FPSLimit")->GetValueNow();
+	}
+	break;
+	case 2:
+	{
+		this->m_Param[0].m_Str = std::to_string(pOption->GetParam("XSensing")->GetSelect());
+		this->m_Param[1].m_Str = std::to_string(pOption->GetParam("YSensing")->GetSelect());
+
+		for (int loop = 2; loop < this->m_NowTabMax; ++loop) {
+			this->m_Param[loop].m_Str = KeyParam::GetKeyStr(KeyParam::Instance()->GetKeyAssign(static_cast<EnumBattle>(loop - 2), 0));
+		}
+	}
+	break;
+	case 3:
+		break;
+	default:
+		break;
+	}
+	for (int loop = 0; loop < this->m_NowTabMax; ++loop) {
+		auto& param = this->m_Param[loop];
+		this->m_DrawUI->Get(param.m_ID).GetParts("String0")->SetString(param.m_Str);
+	}
+}
+void OptionWindow::SetTab() noexcept {
+	std::string ChildBase = "OptionUI/Child";
+	ChildBase += std::to_string(this->m_NowSelectTab + 1);
+	Param2D Param;
+	Param.OfsNoRad = VECTOR2D(980, 205);
+	{
+		std::string Path = "data/UI/Option/Tab";
+		Path += std::to_string(this->m_NowSelectTab + 1);
+		Path += ".json";
+		this->m_DrawUI->AddChild(ChildBase.c_str(), Path.c_str(), Param);
+	}
+	this->m_NowTabMax = this->m_ParamMax;
+	for (int loop = 0; loop < this->m_NowTabMax; ++loop) {
+		auto& param = this->m_Param[loop];
+		std::string Child = ChildBase;
+		Child += "/Column";
+		Child += std::to_string(loop + 1);
+		param.m_ID = this->m_DrawUI->GetID(Child.c_str());
+		if (param.m_ID == -1) {
+			this->m_NowTabMax = loop;
+			break;
+		}
+		param.m_MinID = this->m_DrawUI->GetID((Child + "/Box1").c_str());
+		param.m_MaxID = this->m_DrawUI->GetID((Child + "/Box0").c_str());
+
+		this->m_DrawUI->Get(param.m_ID).GetParts("String1")->SetString(LocalizePool::Instance()->Get(100 * (this->m_NowSelectTab + 1) + loop));
+		if (param.m_MinID != -1) {
+			this->m_DrawUI->Get(param.m_MinID).GetParts("String1")->SetString("<");
+		}
+		if (param.m_MaxID != -1) {
+			this->m_DrawUI->Get(param.m_MaxID).GetParts("String1")->SetString(">");
+			if (this->m_NowSelectTab == 2) {
+				if (loop >= 2) {
+					this->m_DrawUI->Get(param.m_MaxID).GetParts("String1")->SetString("Reset");
+				}
+			}
+		}
+	}
+	UpdateColumnStr();
+	this->m_DrawUI->Get(this->m_TabButton[this->m_NowSelectTab]).SetActive(true);
+}
+void OptionWindow::EndTab() noexcept {
+	std::string Path = "OptionUI/Child";
+	Path += std::to_string(this->m_NowSelectTab + 1);
+	this->m_DrawUI->DeleteChild(Path.c_str());
+	this->m_DrawUI->Get(this->m_TabButton[this->m_NowSelectTab]).SetActive(false);
+}
+void OptionWindow::Init(void) noexcept {
+	this->m_DrawUI = new DrawUISystem();
+
+	this->m_DrawUI->Init("data/UI/Option/OptionBase.json");
+
+	this->m_UIBase = this->m_DrawUI->GetID("");
+	for (int loop = 0; loop < 4; ++loop) {
+		std::string Path = "OptionUI/Tab";
+		Path += std::to_string(loop + 1);
+		this->m_TabButton[loop] = this->m_DrawUI->GetID(Path.c_str());
+		this->m_DrawUI->Get(this->m_TabButton[loop]).GetParts("String1")->SetString(LocalizePool::Instance()->Get(loop));
+		this->m_DrawUI->Get(this->m_TabButton[loop]).SetActive(false);
+	}
+	this->m_NowSelectTab = 0;
+	this->m_CloseButton = this->m_DrawUI->GetID("OptionUI/CloseButton");
+
+	this->m_DrawUI->Get(this->m_UIBase).SetActive(false);
+}
+void OptionWindow::Update(void) noexcept {
+	 auto* KeyMngr = KeyParam::Instance();
+	 if (this->m_DrawUI->Get(this->m_UIBase).IsActive()) {
+		 for (int loop = 0; loop < 4; ++loop) {
+			 if (this->m_DrawUI->Get(this->m_TabButton[loop]).IsSelectButton() && KeyMngr->GetMenuKeyReleaseTrigger(EnumMenu::Diside)) {
+				 if (this->m_NowSelectTab != loop) {
+					 EndTab();
+					 this->m_NowSelectTab = loop;
+					 SetTab();
+				 }
+			 }
+		 }
+		 if (this->m_DrawUI->Get(this->m_CloseButton).IsSelectButton() && KeyMngr->GetMenuKeyReleaseTrigger(EnumMenu::Diside)) {
+			 KeyMngr->Save(KeyMngr->GetLastInputDevice());
+			 SetActive(false);
+		 }
+		 if (KeyMngr->GetMenuKeyRepeat(EnumMenu::Diside)) {
+			 {
+				 auto* pOption = OptionParam::Instance();
+				 switch (this->m_NowSelectTab) {
+				 case 0:
+				 {
+					 //
+					 if (this->m_DrawUI->Get(this->m_Param[0].m_MinID).IsSelectButton()) {
+						 pOption->SetParam("MasterVolume", pOption->GetParam("MasterVolume")->GetSelect() - 1);
+					 }
+					 if (this->m_DrawUI->Get(this->m_Param[0].m_MaxID).IsSelectButton()) {
+						 pOption->SetParam("MasterVolume", pOption->GetParam("MasterVolume")->GetSelect() + 1);
+					 }
+					 //
+					 if (this->m_DrawUI->Get(this->m_Param[1].m_MinID).IsSelectButton()) {
+						 pOption->SetParam("BGMVolume", pOption->GetParam("BGMVolume")->GetSelect() - 1);
+					 }
+					 if (this->m_DrawUI->Get(this->m_Param[1].m_MaxID).IsSelectButton()) {
+						 pOption->SetParam("BGMVolume", pOption->GetParam("BGMVolume")->GetSelect() + 1);
+					 }
+					 //
+					 if (this->m_DrawUI->Get(this->m_Param[2].m_MinID).IsSelectButton()) {
+						 pOption->SetParam("SEVolume", pOption->GetParam("SEVolume")->GetSelect() - 1);
+					 }
+					 if (this->m_DrawUI->Get(this->m_Param[2].m_MaxID).IsSelectButton()) {
+						 pOption->SetParam("SEVolume", pOption->GetParam("SEVolume")->GetSelect() + 1);
+					 }
+				 }
+				 break;
+				 case 1:
+				 {
+					 //
+					 if (this->m_DrawUI->Get(this->m_Param[0].m_MinID).IsSelectButton()) {
+						 pOption->SetParam("WindowMode", pOption->GetParam("WindowMode")->GetSelect() - 1);
+					 }
+					 if (this->m_DrawUI->Get(this->m_Param[0].m_MaxID).IsSelectButton()) {
+						 pOption->SetParam("WindowMode", pOption->GetParam("WindowMode")->GetSelect() + 1);
+					 }
+					 //
+					 if (this->m_DrawUI->Get(this->m_Param[1].m_MinID).IsSelectButton()) {
+						 pOption->SetParam("VSync", pOption->GetParam("VSync")->GetSelect() - 1);
+					 }
+					 if (this->m_DrawUI->Get(this->m_Param[1].m_MaxID).IsSelectButton()) {
+						 pOption->SetParam("VSync", pOption->GetParam("VSync")->GetSelect() + 1);
+					 }
+					 //
+					 if (this->m_DrawUI->Get(this->m_Param[2].m_MinID).IsSelectButton()) {
+						 pOption->SetParam("FPSLimit", pOption->GetParam("FPSLimit")->GetSelect() - 1);
+					 }
+					 if (this->m_DrawUI->Get(this->m_Param[2].m_MaxID).IsSelectButton()) {
+						 pOption->SetParam("FPSLimit", pOption->GetParam("FPSLimit")->GetSelect() + 1);
+					 }
+					 //
+					 MainDraw::Instance()->FlipSetting();
+				 }
+				 break;
+				 case 2:
+				 {
+					 //
+					 if (this->m_DrawUI->Get(this->m_Param[0].m_MinID).IsSelectButton()) {
+						 pOption->SetParam("XSensing", pOption->GetParam("XSensing")->GetSelect() - 1);
+					 }
+					 if (this->m_DrawUI->Get(this->m_Param[0].m_MaxID).IsSelectButton()) {
+						 pOption->SetParam("XSensing", pOption->GetParam("XSensing")->GetSelect() + 1);
+					 }
+					 //
+					 if (this->m_DrawUI->Get(this->m_Param[1].m_MinID).IsSelectButton()) {
+						 pOption->SetParam("YSensing", pOption->GetParam("YSensing")->GetSelect() - 1);
+					 }
+					 if (this->m_DrawUI->Get(this->m_Param[1].m_MaxID).IsSelectButton()) {
+						 pOption->SetParam("YSensing", pOption->GetParam("YSensing")->GetSelect() + 1);
+					 }
+					 //
+					 for (int loop = 2; loop < this->m_NowTabMax; ++loop) {
+						 if (this->m_DrawUI->Get(this->m_Param[loop].m_MaxID).IsSelectButton()) {
+							 EnumBattle Battle = static_cast<EnumBattle>(loop - 2);
+							 KeyMngr->AssignBattleID(Battle, 0, KeyMngr->GetDefaultKeyAssign(Battle, 0));
+						 }
+					 }
+				 }
+				 break;
+				 case 3:
+					 break;
+				 default:
+					 break;
+				 }
+			 }
+			 UpdateColumnStr();
+		 }
+		 if (this->m_NowSelectTab == 2) {
+			 bool IsChange = false;
+			 for (int loop = 2; loop < this->m_NowTabMax; ++loop) {
+				 EnumBattle Battle = static_cast<EnumBattle>(loop - 2);
+				 if (this->m_DrawUI->Get(this->m_Param[loop].m_ID).IsSelectButton()) {
+					 for (int loop2 = static_cast<int>(EnumInput::Begin); loop2 < static_cast<int>(EnumInput::Max); ++loop2) {
+						 EnumInput Input = static_cast<EnumInput>(loop2);
+						 if (KeyMngr->GetKeyPress(Input, true)) {
+							 KeyMngr->AssignBattleID(Battle, 0, Input);
+							 IsChange = true;
+							 break;
+						 }
+					 }
+				 }
+				 if (IsChange) { break; }
+			 }
+			 if (IsChange || KeyMngr->IsDeviceChange()) {
+				 UpdateColumnStr();
+			 }
+		 }
+	 }
+	 this->m_DrawUI->Update();
+	}
